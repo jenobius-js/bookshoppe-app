7859c87862e5f7ac7f617d19b1f9c36e9587bf2a
Jenobius Shamila J <114422664+jenobius-js@users.noreply.github.com>
2023-10-08 03:29:30 +0530

    Update apps.py

--- BookShoppeApp/BookShoppe/apps.py
+++ BookShoppeApp/BookShoppe/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BookshoppeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    app_name = 'BookShoppe'
+    name = 'BookShoppe'
